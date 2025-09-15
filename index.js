898a938c0596eb0fe5be2b938cc1f86b35a0d52a
Akshara Reddy <aksharavooturi25@gmail.com>
2025-09-15 20:37:26 +0530

    changed api request

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 document.getElementById("getAdvice").addEventListener("click", getadvice);
 
-function getadvice() {
-  fetch("https://api.api-ninjas.com/v1/advice", {
-    method: "GET",
-    headers: {
-      'X-Api-Key': 'R4E+sacdvnxhAJy6dNl0/w==4UEFvxxh5bPVbrH5'  
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("advice").textContent = data.advice;
-    })
-    .catch(error => {
-      console.error("Error fetching advice:", error);
-    });
+
+async function getadvice(){
+   try{
+    let res=await fetch("https://api.api-ninjas.com/v1/advice",
+      {
+        headers : {
+          'X-Api-Key': 'R4E+sacdvnxhAJy6dNl0/w==4UEFvxxh5bPVbrH5'  
+        }
+      }
+    );
+    
+    console.log(res);
+     let data = await res.json();
+    document.getElementById("advice").textContent = data.advice;
+   }
+   catch(error){
+    console.log(error);
+   }
 }
